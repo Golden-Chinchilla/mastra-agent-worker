1b7c2769e331f4dacb402b792111ed90a5c1fc97
David <gaojieli2020@gmail.com>
2025-11-29 14:16:29 +0800

    update

--- src/mastra/agents/gasAgent.ts
+++ src/mastra/agents/gasAgent.ts
@@ -5,6 +5,7 @@ import { Agent } from '@mastra/core/agent';
 import { gasTool } from '../tools/gasTool.ts';
 
 export const cryptoAgent = new Agent({
+  id: 'cryptoAgent',
   name: 'Gas Agent',
   instructions: `
       你是一个专注于加密货币研究的Agent。
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,20 +1,15 @@
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-// import { LibSQLStore } from '@mastra/libsql';
-import { cryptoAgent } from './agents/gasAgent';
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { cryptoAgent } from "./agents/gasAgent";
 import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
-// import 'dotenv/config';
 
 export const mastra = new Mastra({
   agents: { cryptoAgent },
-  // storage: new LibSQLStore({
-  //   url: ":memory:",
-  // }),
   logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info",
   }),
   deployer: new CloudflareDeployer({
     projectName: "hello-mastra",
-  })
+  }),
 });
